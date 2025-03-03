c927de80a5d54707abfbac1a62c1cc1a00662213
Sarwar Habib <engr.sarwarhabib@gmail.com>
2025-03-04 00:18:24 +0600

    many color added

--- scripts/themeBtn.js
+++ scripts/themeBtn.js
@@ -3,7 +3,11 @@ document.getElementById('btn-theme').addEventListener('click', function(event){
 
     // event.preventDefault();
     
-    const colorArray = ['yellow', 'indianred', 'purple', 'blue', 'yellowgreen', 'black'];
+    const colorArray = ['yellow', 'indianred', 'purple', 'blue', 'yellowgreen',
+        'AliceBlue', 'AntiqueWhite', 'Aqua', 'Aquamarine', 'Azure', 'Beige', 'Bisque', 'BlanchedAlmond', 'Blue', 'BlueViolet', 'Brown', 'BurlyWood', 'CadetBlue',	'Chartreuse', 'Chocolate', 'Coral', 'CornflowerBlue', 'Cornsilk', 'Crimson', 'Cyan', 'DarkBlue', 'DarkCyan', 'DarkGoldenRod', 'DarkGray', 'DarkGrey', 'DarkGreen'
+    ];
+    
+    // DarkKhaki	DarkMagenta	DarkOliveGreen	Darkorange	DarkOrchid	DarkRed	DarkSalmon	DarkSeaGreen	DarkSlateBlue	DarkSlateGray	DarkSlateGrey	DarkTurquoise	DeepPink	DeepSkyBlue	DimGray	DimGrey	DodgerBlue	FireBrick	FloralWhite	ForestGreen	Fuchsia	Gainsboro	GhostWhite	Gold	GoldenRod	Gray	Grey	Green	GreenYellow	HoneyDew	HotPink	IndianRed	Indigo	Ivory	Khaki	Lavender	LavenderBlush	LawnGreen	LemonChiffon	LightBlue	LightCoral	LightCyan	LightGoldenRodYellow	LightGray	LightGrey	LightGreen	LightPink	LightSalmon	LightSeaGreen	LightSkyBlue	LightSlateGray	LightSlateGrey	LightSteelBlue	LightYellow	Lime	LimeGreen	Linen	Magenta	Maroon	MediumAquaMarine	MediumBlue	MediumOrchid	MediumPurple	MediumSeaGreen	MediumSlateBlue	MediumSpringGreen	MediumTurquoise	MediumVioletRed	MidnightBlue	MintCream	MistyRose	Moccasin	NavajoWhite	Navy	OldLace	Olive	OliveDrab	Orange	OrangeRed	Orchid	PaleGoldenRod	PaleGreen	PaleTurquoise	PaleVioletRed	PapayaWhip	PeachPuff	Peru	Pink	Plum	PowderBlue	Purple	Red	RosyBrown	RoyalBlue	SaddleBrown	Salmon	SandyBrown	SeaGreen	SeaShell	Sienna	Silver	SkyBlue	SlateBlue	SlateGray	SlateGrey	Snow	SpringGreen	SteelBlue	Tan	Teal	Thistle	Tomato	Turquoise	Violet	Wheat	White	WhiteSmoke	Yellow	YellowGreen
     
     const randomNumber = Math.floor(Math.random() * colorArray.length);
     console.log(randomNumber);
@@ -12,6 +16,6 @@ document.getElementById('btn-theme').addEventListener('click', function(event){
 
     // }
 
-    document.getElementById('body').style.backgroundColor = "bg-yellow-500";
-    // document.getElementById('body').style.backgroundColor = colorArray[randomNumber];
+    // document.getElementById('body').style.backgroundColor = "purple";
+    document.getElementById('body').style.backgroundColor = colorArray[randomNumber];
 })
